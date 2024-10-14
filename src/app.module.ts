602f2e13488f5764f056b0811321ed0e19801ebb
0x7d202060618 <tymothycromer1@outlook.com>
2024-10-14 21:21:05 +0200

    feat: init prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { PriceModule } from './price/price.module';
 import { PairController } from './pair/pair.controller';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [HttpModule, PriceModule],
   controllers: [PairController],
+  providers: [PrismaService],
 })
 export class AppModule {}
